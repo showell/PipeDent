556401e5c627f612914a911c6c4db13fa200130a
Steve Howell <steve@strings.com>
2011-07-04 09:39:18 -0700

    remove test code for now

--- test.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  var input, pipedent;
-  input = 'html\n  head\n  body\n    table\n      tr\n        td id="one" | one\n        td id="two" | two\n    p\n      | This is just free\n      | form text.\n    div class="body"\n      <b>raw html just passes thru and takes precedence over | </b>\n    span | Hello world';
-  pipedent = require('./pipedent');
-  console.log(pipedent.convert(input));
-}).call(this);
