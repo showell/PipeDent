a5742e70932ed1c295e9725a5c47e596c213ec15
Grockit <grockit@d.local>
2011-08-11 14:46:15 -0700

    demo improvements

--- demo.coffee
+++ demo.coffee
@@ -1,29 +1,44 @@
-my_html_input = \
+demo_layout = \
   '''
   p
-    | Welcome the demo.  As you edit the code on the left, it
+    | Welcome to the demo.  As you edit the code on the left, it
     | will convert automatically.
   table
-    tr
+    tr valign="top"
       td
         h1 | Input
         textarea id="input" rows=80 cols=80
           PASS
       td
         h1 | Output HTML
-        textarea id="output" rows=80 cols=80
+        textarea id="output" rows=20 cols=80
           PASS
+        h1 | Rendered HTML
+        div id="rendered"
+          PASS
+  '''
+
+my_html_input = \
+  '''
+  table border=1
+    tr
+      td | "NW"
+      td | "NE"
+    tr
+      td | "SW"
+      td | "SE"
   '''
 
 demo_input = my_html_input
 convert = this.pipedent_convert
   
 $(document).ready ->
+  $("#content").html(convert demo_layout)
   demo_output = convert(demo_input)  
-  $("#content").html(convert my_html_input)
-  console.log(demo_output)
+  $("#input").tabby {tabString: "  "};
   $("#input").text(demo_input)
   $("#output").text(demo_output)
+  $("#rendered").html(demo_output)
   last_parsed_text = demo_input
   # This is a crude mechanism to continually parse
   # the input.
@@ -32,6 +47,7 @@ $(document).ready ->
     if input != last_parsed_text
       output = convert(input)
       $("#output").text(output)
+      $("#rendered").html(output)
       last_parsed_text = input
     setTimeout parse, 200
   setTimeout parse, 200
--- demo.js
+++ demo.js
@@ -1,15 +1,19 @@
 (function() {
-  var convert, demo_input, my_html_input;
-  my_html_input = 'p\n  | Welcome the demo.  As you edit the code on the left, it\n  | will convert automatically.\ntable\n  tr\n    td\n      h1 | Input\n      textarea id="input" rows=80 cols=80\n        PASS\n    td\n      h1 | Output HTML\n      textarea id="output" rows=80 cols=80\n        PASS';
+  var convert, demo_input, demo_layout, my_html_input;
+  demo_layout = 'p\n  | Welcome to the demo.  As you edit the code on the left, it\n  | will convert automatically.\ntable\n  tr valign="top"\n    td\n      h1 | Input\n      textarea id="input" rows=80 cols=80\n        PASS\n    td\n      h1 | Output HTML\n      textarea id="output" rows=20 cols=80\n        PASS\n      h1 | Rendered HTML\n      div id="rendered"\n        PASS';
+  my_html_input = 'table border=1\n  tr\n    td | "NW"\n    td | "NE"\n  tr\n    td | "SW"\n    td | "SE"';
   demo_input = my_html_input;
   convert = this.pipedent_convert;
   $(document).ready(function() {
     var demo_output, last_parsed_text, parse;
+    $("#content").html(convert(demo_layout));
     demo_output = convert(demo_input);
-    $("#content").html(convert(my_html_input));
-    console.log(demo_output);
+    $("#input").tabby({
+      tabString: "  "
+    });
     $("#input").text(demo_input);
     $("#output").text(demo_output);
+    $("#rendered").html(demo_output);
     last_parsed_text = demo_input;
     parse = function() {
       var input, output;
@@ -17,6 +21,7 @@
       if (input !== last_parsed_text) {
         output = convert(input);
         $("#output").text(output);
+        $("#rendered").html(output);
         last_parsed_text = input;
       }
       return setTimeout(parse, 200);
